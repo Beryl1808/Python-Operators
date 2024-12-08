822ebe6c29d8d489c5388793608a5912783c4381
Beryl1808 <dkwafo17@gmail.com>
2024-12-08 19:30:01 +0000

    1commit

---
+++ Average.py
@@ -0,0 +1,13 @@
+#Storing  Values
+tree1 = 10
+tree2 = 90
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+#
+sum= tree1+tree2+tree3+tree4+tree5
+print("the sum of  all the 5trees is:" ,sum)
+
+average= sum/5
+print("the average of  all the 5trees is:" ,average)
